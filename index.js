62f514a4252d01586a814c20a4d85971fa3486ad
rftgyhui8 <gs1techpaul@hotmail.com>
2024-05-13 07:42:45 +0700

    hi commit

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const gcdRecursive = (a, b) => {
-  if (b === 0) {
-    return a;
+function addTwoNumbers(l1, l2) {
+  const dummy = new ListNode(0);
+  let p = l1,
+    q = l2,
+    curr = dummy;
+  let carry = 0;
+  while (p !== null || q !== null) {
+    const x = p !== null ? p.val : 0;
+    const y = q !== null ? q.val : 0;
+    const sum = x + y + carry;
+    carry = Math.floor(sum / 10);
+    curr.next = new ListNode(sum % 10);
+    curr = curr.next;
+    if (p !== null) p = p.next;
+    if (q !== null) q = q.next;
   }
-  return gcdRecursive(b, a % b);
-};
+  if (carry > 0) {
+    curr.next = new ListNode(carry);
+  }
+  return dummy.next;
+}
